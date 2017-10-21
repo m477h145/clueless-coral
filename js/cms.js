5311264c6095253525d017928a253840fba05cea
Jacob Mackenzie-Websdale <fletch6@talktalk.net>
2017-10-21 10:23:01 +0100

    Fixed icon, added display_post function and safety.
    Fixed the icon in the footer :smile: so it aligns with text.
    Added display_post function in cms.js.

--- js/cms.js
+++ js/cms.js
@@ -10,16 +10,17 @@ var config = {
 firebase.initializeApp(config);
 
 // Functions
-/// write_post:  Write a post, returns an ID (string).
-/// read_post:   Read a post, returns a JSON object (teacher, title, text, club, picture)
-/// update_post: Update a post, returns a boolean, whether it was successful or not.
+/// write_post:   Write a post, returns an ID (string).
+/// read_post:    Read a post, returns a JSON object (teacher, title, text, club, picture)
+/// update_post:  Update a post, returns a boolean, whether it was successful or not.
+/// display_post: Display a post in the HTML element given by the second argument, returns null.
 function write_post(teacher, title, text, club, picture) {
   key = firebase.database().ref().child('posts').push().key;
   firebase.database().ref('posts/' + key).set({
-    teacher: teacher,
+    teacher: teacher || 'Anonymous',
     title: title,
     text: text,
-    club: club || "School",
+    club: club || 'School',
     picture: picture || null
   });
   return key;
@@ -47,4 +48,21 @@ function update_post(key, teacher, title, text, club, picture) {
     })
     return true;
   }
+}
+
+function display_post(key, parent) {
+  post = read_post(key);
+  div = document.createElement('div');
+  div.innerHTML = '<div class="card text-white bg-primary mb-3" style="max-width: 33.333333%;">' +
+  '<div class="card-header">' + post.club ' / ' + post.teacher + '</div>';
+  if (post.picture !== null) {
+    div.innerHTML += '<img class="card-img-top" src="' + post.picture + '" alt=""' post.title ' / Image"">';
+  }
+  div.innerHTML += '<div class="card-body">' +
+  '<h4 class="card-title">' + post.title + '</h4>' +
+  '<p class="card-text">' + post.text + '</p>' +
+  '</div>' +
+  '</div>';
+  parent.appendChild(div);
+  return null;
 }
